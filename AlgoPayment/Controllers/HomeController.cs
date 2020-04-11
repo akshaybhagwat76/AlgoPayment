719efa997ffe5a20b38ca9cd0069e6cea5acbe78
Akshay Bhagwat <akshaybhagwat76@gmail.com>
2020-04-11 16:54:50 +0530

    added project

---
+++ AlgoPayment/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using AlgoPayment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlgoPayment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SocialLogin(UserDetail data)
+        {
+            if (data != null)
+            {
+                using (eponym_app_licenseEntities db = new eponym_app_licenseEntities())
+                {
+                    if (!IsUserExist(data.Mobile))
+                    {
+                        db.UserDetails.Add(data);
+                        db.SaveChanges();
+                    }
+                }
+            }
+            return Json("Added", JsonRequestBehavior.AllowGet);
+        }
+
+        public bool IsUserExist(string id)
+        {
+            using (eponym_app_licenseEntities db =new eponym_app_licenseEntities())
+            {
+                return db.UserDetails.Count(x => x.Mobile == id) > 0;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public ActionResult AddUser(UserDetail user)
+        {
+            if (user != null)
+            {
+                using (eponym_app_licenseEntities db= new eponym_app_licenseEntities())
+                {
+                    db.UserDetails.Add(user);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
